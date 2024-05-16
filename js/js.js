94921d758dbb4c701cf7618d61d30cd43cde526c
gustavoch <chiappegus@gmial.com>
2024-05-15 22:28:31 -0300

    "Se agragó js conteo de pizzas"

---
+++ js/js.js
@@ -0,0 +1,64 @@
+function muzzarella() {
+    valor=document.getElementById("muzzarella_count").value
+    console.log(valor)
+    valor=1+parseInt(valor)
+    console.log(valor)
+    document.getElementById("muzzarella_count").value=valor
+
+}
+
+
+
+
+function napolitana() {
+    valor=document.getElementById("napolitana_count").value
+    console.log(valor)
+    valor=1+parseInt(valor)
+    console.log(valor)
+    document.getElementById("napolitana_count").value=valor
+
+}
+
+
+function quesos() {
+    valor=document.getElementById("quesos_count").value
+    console.log(valor)
+    valor=1+parseInt(valor)
+    console.log(valor)
+    document.getElementById("quesos_count").value=valor
+
+}
+
+
+
+function anchoas() {
+    valor=document.getElementById("anchoas_count").value
+    console.log(valor)
+    valor=1+parseInt(valor)
+    console.log(valor)
+    document.getElementById("anchoas_count").value=valor
+
+}
+
+
+
+function calabresa() {
+    valor=document.getElementById("calabresa_count").value
+    console.log(valor)
+    valor=1+parseInt(valor)
+    console.log(valor)
+    document.getElementById("calabresa_count").value=valor
+
+}
+
+
+function jamonYmorron() {
+    valor=document.getElementById("jamonYmorron_count").value
+    console.log(valor)
+    valor=1+parseInt(valor)
+    console.log(valor)
+    document.getElementById("jamonYmorron_count").value=valor
+
+}
+
+
